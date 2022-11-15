54e1e3170ccec13388ca32556e5c25ba67f78814
Lucas Diaz <lucasgustavodiaz@hotmail.com>
2022-11-15 10:46:44 -0300

    toggle theme

---
+++ src/components/icons/logo.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function LogoIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 218 200"
+      className="w-5 inline-block transition-transform group-hover:rotate-[30deg]"
+    >
+      <polygon
+        points="0 65 23.31 32.1 108 165 119 148 141.19 182.77 130 200 86 200 0 65"
+        // fill="#fda909"
+        className="fill-[#8f8e8e] dark:fill-[#fda909]"
+      />
+      <path
+        d="M174,0H46L36,14.09,142.47,180.85,218,65ZM143,114,95,37h60l19,28Z"
+        // fill="#00a6ea"
+        className="fill-[#141414] dark:fill-[#00a6ea]"
+      />
+    </svg>
+  )
+}
