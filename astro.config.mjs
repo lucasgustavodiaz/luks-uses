e494565fa1174ab1604a0b94744b2ad107fedac0
Lucas Diaz <lucasgustavodiaz@hotmail.com>
2022-12-07 12:47:06 -0300

    fix deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import addClasses from 'rehype-add-classes'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astronaut.github.io',
+  site: 'https://lucasgustavodiaz.github.io',
   base: '/luks-uses',
   integrations: [
     sitemap(),
